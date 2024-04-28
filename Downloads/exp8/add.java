3bc3a5332d8a00992b74451e59b514347d07c7fc
anisha <anisha.03bhole@gmail.com>
2024-04-28 20:36:28 +0530

    first commit

---
+++ Downloads/exp8/add.java
@@ -0,0 +1,9 @@
+public class add{
+    public static void main(String[]args){
+        int fnum =20;
+        int snum = 10;
+
+        int sum = fnum+snum;
+        System.out.println(fnum + "+" + snum +"=" + sum);
+    }
+}
